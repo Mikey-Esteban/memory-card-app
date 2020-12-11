1ca8eb19df25429768227760693b167cb6022c06
Mikey <michaelp.esteban@gmail.com>
2020-12-11 18:08:56 -0500

    edited nav/ footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <nav className="font-sans flex text-center justify-center py-2 px-6 bg-white shadow sm:items-baseline w-full">
+    <nav className="font-sans flex text-center justify-center py-4 px-6 bg-white shadow sm:items-baseline w-full">
       <div className="mb-2 sm:mb-0 text-center">
-        <a href="/home" className="text-sm no-underline text-gray-700 hover:text-blue-900">Site by Mikey Esteban</a>
+        <p className="text-sm text-blue-900">Site by Mikey Esteban</p>
       </div>
     </nav>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,8 @@ const Navbar = ({resetGame}) => {
   return (
     <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
       <div className="mb-2 sm:mb-0">
-        <a href="/home" className="text-lg no-underline text-gray-700 hover:text-blue-dark">Code on Github</a>
+        <a href="https://github.com/Mikey-Esteban/memory-card-app" className="text-lg no-underline text-gray-500 hover:text-blue-900">Github code</a>
+        <a href="https://www.theodinproject.com/courses/javascript/lessons/memory-card" className="text-lg no-underline text-gray-500 hover:text-blue-900"> | Project from TOP</a>
       </div>
       <div>
         <button className="bg-transparent hover:bg-blue-900 text-blue-900 font-semibold hover:text-white py-2 px-4 border border-blue-900 hover:border-transparent rounded"
